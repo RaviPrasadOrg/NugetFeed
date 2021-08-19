27822dd6366d99c3f7509b24cbc638eeeada1094
Ravi Ranjan Prasad <87794332+RaviRanjan-Prasad@users.noreply.github.com>
2021-08-19 12:55:51 +0530

    initial checkin

---
+++ NugetFeed/HelloWorld/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class HelloWorld
+    {
+        private string _message = "Hellow from feed";
+
+        public string Message
+        {
+            get { return _message; }
+        }
+    }
+}
