de637c8c9415d61f44ce936af7d39241d2f1292b
Debabrata Maji <debabrata.ma@travelport.com>
2021-08-20 12:25:29 +0530

    added new projects in .net framework 4.8

---
+++ NugetFeedTest/HelloWorld/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class HelloWorld
+    {
+        private string _message = "Hellow from feed";
+
+        public string Message
+        {
+            get { return _message; }
+        }
+    }
+}
