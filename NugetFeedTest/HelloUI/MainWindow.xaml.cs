2c1608bbb4a70c4975cef9d6947dcd0e40fda084
Debabrata Maji <debabrata.ma@travelport.com>
2021-08-30 22:39:12 +0530

    deleted class library

--- NugetFeedTest/HelloUI/MainWindow.xaml.cs
+++ NugetFeedTest/HelloUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NugetTestLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,7 @@ namespace HelloUI
 
         private void OnButtonClick(object sender, RoutedEventArgs e)
         {
-
+            Class1 clss = new Class1();            
         }
     }
 }
